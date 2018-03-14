746743d4601829494a9a0b04d70bf02bf31f2f03
Thomas Creamer <tcream24@gmail.com>
2018-03-13 23:16:19 -0400

    added ejs template engine, views and routes folder, and json data file. Renders view with data from data.json

--- index.js
+++ index.js
@@ -6,9 +6,24 @@ let fs = require('fs');
 let app = express();
 
 app.set('port', process.env.PORT || 3000);
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+app.engine('html', require('ejs').renderFile);
 
-app.get('/', (req, res) => res.send('hello world!'));
+app.get('/', (req, res) => {
+  let data = {};
+  fs.readFile('data.json', 'utf8', (err, result) => {
+    if (err) {
+      console.log(err);
+       res.render('index.ejs', {data: "no data"});
+    } else {
+      data = JSON.stringify(result);
+      res.render('index.ejs', {data: data});
+    }
+  })
+});
 
-http.createServer(app).listen(app.get('port'), '0.0.0.0', function() {
-  console.log('Server running on port ' + app.get('port'));
-})
+http.createServer(app)
+  .listen(app.get('port'), '0.0.0.0', () => {
+    console.log('Server running on port ' + app.get('port'));
+  })
