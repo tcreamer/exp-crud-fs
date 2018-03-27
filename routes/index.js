efe764da1cb4f4936a3bf52052aa97914e13204c
Thomas Creamer <tcream24@gmail.com>
2018-03-27 00:20:24 -0400

    added missing catch statements for routes

--- routes/index.js
+++ routes/index.js
@@ -15,16 +15,19 @@ function index(req, res){
 function post(req, res) {
   API.POST(req.body.itemInput)
   .then(() => res.redirect('/'))
+  .catch(err => console.log(err));
 }
 
 function completed(req, res) {
   API.UPDATE(req.params.id)
   .then(() => res.redirect('/'))
+  .catch(err => console.log(err));
 }
 
 function deleted(req, res) {
   API.DELETE(req.params.id)
   .then(() => res.redirect('/'))
+  .catch(err => console.log(err));
 }
 
 module.exports = {
